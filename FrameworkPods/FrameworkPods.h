1426b4bdaae1007346bf12cf1a62f8c3855bf719
RameshKhanna <kamalinikhanna2006@gmail.com>
2021-09-30 13:04:23 +0530

    add files

---
+++ FrameworkPods/FrameworkPods.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkPods.h
+//  FrameworkPods
+//
+//  Created by Ramesh khanna on 30/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkPods.
+FOUNDATION_EXPORT double FrameworkPodsVersionNumber;
+
+//! Project version string for FrameworkPods.
+FOUNDATION_EXPORT const unsigned char FrameworkPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkPods/PublicHeader.h>
+
+
