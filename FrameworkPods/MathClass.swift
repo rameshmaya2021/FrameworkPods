edc8b61eb27019de0779f6ba0e64b07c767943b3
RameshKhanna <kamalinikhanna2006@gmail.com>
2021-09-30 13:38:17 +0530

    Added test cases

---
+++ FrameworkPods/MathClass.swift
@@ -0,0 +1,17 @@
+//
+//  MathClass.swift
+//  FrameworkPods
+//
+//  Created by Ramesh khanna on 30/09/21.
+//
+
+import Foundation
+
+public class MathClass {
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- FrameworkPodsTests/FrameworkPodsTests.swift
+++ FrameworkPodsTests/FrameworkPodsTests.swift
@@ -9,25 +9,22 @@ import XCTest
 @testable import FrameworkPods
 
 class FrameworkPodsTests: XCTestCase {
+    var mathClass: MathClass?
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mathClass = MathClass()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mathClass = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAdd() {
+        XCTAssertEqual(mathClass?.add(a: 2, b: 2), 4)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(mathClass?.sub(a: 2, b: 2), 0)
     }
 
 }
